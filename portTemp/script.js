00a16cc538c5a5dd0fb8299e3120baf360de8793
César Diego Anovich <iddiegobis@gmail.com>
2025-04-11 17:54:26 -0300

    add

--- portTemp/script.js
+++ portTemp/script.js
@@ -1,9 +1,8 @@
+const menuToggle = document.getElementById('menuToggle');
+const sideMenu = document.getElementById('sideMenu');
 
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  sideMenu.classList.toggle('active');
+});
 
-  const menuToggle = document.getElementById('menuToggle');
-  const sideMenu = document.getElementById('sideMenu');
-
-  menuToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle('active');
-    sideMenu.classList.toggle('active');
-  });
