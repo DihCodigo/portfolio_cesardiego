75ba58c5ed46a5c8d83a558b5265299a56e6b7de
César Diego Anovich <iddiegobis@gmail.com>
2025-04-14 18:19:03 -0300

    add1

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.getElementById('menuToggle');
+const sideMenu = document.getElementById('sideMenu');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  sideMenu.classList.toggle('active');
+});
+
